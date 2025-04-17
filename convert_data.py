49c305e4b594f5838dda03c62e65d489960878f7
MeriDK <ma.rizhko@gmail.com>
2025-04-16 18:37:10 -0700

    Specified data schema

--- convert_data.py
+++ convert_data.py
@@ -20,15 +20,17 @@ Steps to run:
 """
 
 
-root = Path('./data')
-for ds in ('asassn', 'macho', 'ogle3'):
+root = Path("./data")
+for ds in ("asassn", "macho", "ogle3"):
     print(ds)
     for seed in range(8):
         print(seed)
-        data = joblib.load(root / f'{ds}_filtered.pkl')
-        train_index, test_index = joblib.load(root / 'seeds' / f'{ds}_{seed}' / 'index.pkl')
-        train_idx, _, _, _ = joblib.load(root / 'seeds' / f'{ds}_{seed}' / 'train.pkl')
-        val_idx, _, _, _ = joblib.load(root / 'seeds' / f'{ds}_{seed}' / 'val.pkl')
+        data = joblib.load(root / f"{ds}_filtered.pkl")
+        train_index, test_index = joblib.load(
+            root / "seeds" / f"{ds}_{seed}" / "index.pkl"
+        )
+        train_idx, _, _, _ = joblib.load(root / "seeds" / f"{ds}_{seed}" / "train.pkl")
+        val_idx, _, _, _ = joblib.load(root / "seeds" / f"{ds}_{seed}" / "val.pkl")
 
         train_index = np.stack(train_index)
         test_index = np.stack(test_index)
@@ -36,36 +38,62 @@ for ds in ('asassn', 'macho', 'ogle3'):
         val_idx = np.stack(val_idx)
 
         L = 200
-        train_split = [chunk for i in train_index for chunk in data[i].split(L, L) if data[i].label is not None]
-        test = [chunk for i in test_index for chunk in data[i].split(L, L) if data[i].label is not None]
+        train_split = [
+            chunk
+            for i in train_index
+            for chunk in data[i].split(L, L)
+            if data[i].label is not None
+        ]
+        test = [
+            chunk
+            for i in test_index
+            for chunk in data[i].split(L, L)
+            if data[i].label is not None
+        ]
 
         train = [train_split[i] for i in train_idx]
         val = [train_split[i] for i in val_idx]
 
-        train_df = pl.DataFrame({
-            'mjd': [el.times for el in train],
-            'mag': [el.measurements for el in train],
-            'magerr': [el.errors for el in train],
-            'period': [el.p for el in train],
-            'label': [el.label for el in train]
-        })
-        val_df = pl.DataFrame({
-            'mjd': [el.times for el in val],
-            'mag': [el.measurements for el in val],
-            'magerr': [el.errors for el in val],
-            'period': [el.p for el in val],
-            'label': [el.label for el in val]
-        })
-        test_df = pl.DataFrame({
-            'mjd': [el.times for el in test],
-            'mag': [el.measurements for el in test],
-            'magerr': [el.errors for el in test],
-            'period': [el.p for el in test],
-            'label': [el.label for el in test]
-        })
+        schema = {
+            "mjd": pl.List(pl.Float32),
+            "mag": pl.List(pl.Float32),
+            "magerr": pl.List(pl.Float32),
+            "period": pl.Float32,
+            "label": pl.Utf8,
+        }
+        train_df = pl.DataFrame(
+            {
+                "mjd": [el.times for el in train],
+                "mag": [el.measurements for el in train],
+                "magerr": [el.errors for el in train],
+                "period": [el.p for el in train],
+                "label": [el.label for el in train],
+            },
+            schema=schema,
+        )
+        val_df = pl.DataFrame(
+            {
+                "mjd": [el.times for el in val],
+                "mag": [el.measurements for el in val],
+                "magerr": [el.errors for el in val],
+                "period": [el.p for el in val],
+                "label": [el.label for el in val],
+            },
+            schema=schema,
+        )
+        test_df = pl.DataFrame(
+            {
+                "mjd": [el.times for el in test],
+                "mag": [el.measurements for el in test],
+                "magerr": [el.errors for el in test],
+                "period": [el.p for el in test],
+                "label": [el.label for el in test],
+            },
+            schema=schema,
+        )
 
-        out_path = root / 'parquet' / ds / str(seed)
+        out_path = root / "parquet" / ds / str(seed)
         out_path.mkdir(parents=True, exist_ok=True)
-        train_df.write_parquet(out_path / 'train.parquet')
-        val_df.write_parquet(out_path / 'val.parquet')
-        test_df.write_parquet(out_path / 'test.parquet')
+        train_df.write_parquet(out_path / "train.parquet")
+        val_df.write_parquet(out_path / "val.parquet")
+        test_df.write_parquet(out_path / "test.parquet")
